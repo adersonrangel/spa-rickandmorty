7bc8abab4ca64302f5780313c15a981c161bc33b
Aderson Rangel <adersonrangel@gmail.com>
2020-07-17 11:14:09 -0500

    [Add] Router to the application

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+const Home = () => {
+  const view = `
+    <div class="characters">
+        <article class="character-item">
+            <a href="#/1">
+            <img src="#/1" alt="name" />
+            <h2>Name</h2>
+            </a>
+        </article>
+    </div>
+    `;
+  return view;
+};
+
+export default Home;
---
+++ src/templates/Header.js
@@ -0,0 +1,18 @@
+const Header = () => {
+  const view = `
+       <div class="header-main">
+           <div class="header-logo">
+               <h1>
+                   <a href="/">100tifi.co</a>
+               </h1>
+           </div>
+       </div>
+       <div class="header-nav">
+           <a href="#/about/">About</a>
+       </div>
+    `;
+  debugger;
+  return view;
+};
+
+export default Header;
